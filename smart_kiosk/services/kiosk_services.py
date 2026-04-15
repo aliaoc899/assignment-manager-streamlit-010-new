8bff2d5762969903d3220888e2714fcd79d9f87e
Ali Simaei <aliaocc@Alis-MacBook-Pro.local>
2026-04-15 19:16:53 -0400

    smart-kiosk

--- smart_kiosk/services/kiosk_services.py
+++ smart_kiosk/services/kiosk_services.py
@@ -1,32 +1,32 @@
 import uuid
-#from typing import List, Dict, Optional
+from typing import List, Dict, Optional
 
-
-
-#Query 1: Place a new order for an item and quantity.
-#Query 2: View all orders placed for a particular item.
-#Quer4: find how many orders placed for the item using item id
-
-def place_order(inventory_items : list, item_id : str, quantity: int, orders: list):
-    # find item
+def place_order(inventory_items : list, item_id : str, quantity: int, orders: list) -> Optional[Dict]:
     item = find_inventory_item_by_item_id(inventory_items, item_id)
-    
-    # check the existing invetory for the item.
     if item:
-        if item['quantity'] >= quantity:
-            orders.append(
-                {
-                    "order_id": str(uuid.uuid4())   
-                }
-            )
-            # if we have enough inventory then reduce the invetory
+        if item['stock'] >= quantity:
+            item['stock'] = item["stock"] - quantity #reduce the stock
 
-            # then place the order
-            
+            total = quantity * item['unit_price']
 
-    
+            # create the new order dict
+            new_order = {
+                "order_id": str(uuid.uuid4()),
+                "item_id": item_id,
+                "quantity": quantity,
+                "status": "placed",
+                "total": total
+            }
+            #add the new order to the orders
+            orders.append(new_order)
+            return new_order
 
- 
+
+def find_item_names(inventory: List) -> List:
+    item_names = []
+    for item in inventory:
+        item_names.append(item['name'])
+    return item_names
 
 def find_orders_by_item_id():
     pass
@@ -34,12 +34,21 @@ def find_orders_by_item_id():
 def count_orders_by_item_id():
     pass
 
-def find_inventory_item_by_item_id(invetory_list: list, item_id : str):
+def find_inventory_item_by_item_id(invetory_list: list, item_id : str) -> Optional[Dict]:
     for item in invetory_list:
-        if item['id'] == item_id:
+        if item['item_id'] == item_id:
             return item
     
     return None
 
 def update_invetory_item():
+    pass
+
+def add_new_item_to_inventory():
+    pass
+
+def update_inventory_item():
+    pass
+
+def cancel_order():
     pass
