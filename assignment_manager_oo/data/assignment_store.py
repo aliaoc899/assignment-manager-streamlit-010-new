3b6f1b4a5c11b7119fa4af73e839d2b396193ee7
Ali Simaei <aliaocc@wifi-roaming-128-4-186-130.wifi.udel.edu>
2026-04-24 12:59:16 -0400

    oo - day 3

--- assignment_manager_oo/data/assignment_store.py
+++ assignment_manager_oo/data/assignment_store.py
@@ -1,12 +1,19 @@
 import json
 from pathlib import Path
 
-class AssignmentStore:
-    def __init__(self) -> None:
-        pass
+from typing import Dict, List, Optional
 
-    def load(self):
-        pass
+class AssignmentStore:
+    def __init__(self, json_path: Path) -> None:
+        self.json_path = json_path
+
+    def load(self) -> List[Dict]:
+        if self.json_path.exists():
+            with open(self.json_path,"r") as f:
+                return json.load(f)
+        else:
+            return []
     
-    def save(self):
-        pass
+    def save(self, assignments: List[Dict]):
+        with open(self.json_path, "w") as f:
+            json.dump(assignments, f)
--- assignment_manager_oo/services/assignment_manager.py
+++ assignment_manager_oo/services/assignment_manager.py
@@ -1,5 +1,6 @@
 
 from typing import List, Dict, Optional
+import uuid
 
 class AssignmentManager:
     def __init__(self,intial_assignments) -> None:
@@ -9,8 +10,24 @@ class AssignmentManager:
         return list(self.assignments)
 
     def add(self, title: str , description: str,
-            points: int , assignment_type: str):
-        pass
+            points: int , assignment_type: str) -> Dict:
+        if not title.strip():
+            raise ValueError("Title is required")
+        
+        allowed_types = ["Homework", "Lab"]
+
+        if assignment_type.lower() not in allowed_types:
+            raise ValueError("assignment type is invalid")
+        
+        new_assignment = {
+                "id": str(uuid.uuid4()),
+                "title" : title,
+                "description": description,
+                "points" : points,
+                "type" : assignment_type.lower()
+        }
+        self.assignments.append(new_assignment)
+        return new_assignment
 
     def delete(self, assignment_id: str):
         pass
