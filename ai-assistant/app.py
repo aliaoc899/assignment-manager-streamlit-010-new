db80b339b2bf9c36914e3ad9cf4121dc5c1fa225
Ali Simaei <aliaocc@wifi-roaming-128-4-178-81.wifi.udel.edu>
2026-05-01 11:26:10 -0400

    ai chatbot design day 2

--- ai-assistant/app.py
+++ ai-assistant/app.py
@@ -1,8 +1,12 @@
 import os
 import streamlit as st
+
 from dotenv import load_dotenv
 from openai import OpenAI
+
+
 from pathlib import Path
+
 import json
 
 load_dotenv()
@@ -21,6 +25,8 @@ clinet = OpenAI(api_key=api_key) # create an object from the open ai class and
                     #                   intialize it with the api key
 
 
+# data layer
+
 def load_orders_data(filepath: str):
     json_path = Path(filepath)
     if json_path.exists():
@@ -29,10 +35,7 @@ def load_orders_data(filepath: str):
     else:
         return []
     
-
-
 orders = load_orders_data("ai-assistant/orders.json")
-
 
 # Chat logs loads and saving methods
 
@@ -49,23 +52,91 @@ def save_logs(filepath, logs):
     with open(json_path, "w") as f:
         json.dump(logs, f)
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+#service layer
+def build_ai_prompt(context: str):
+    return "" \
+    " You are a helpful company assistant. " \
+    "Answeer user questions based on some sample data that you create" \
+    "and return the response to the user. include some data in the response" \
+    "these are the guardrailes: " \
+    "- do not use negative words "\
+    f"this is my context: {context}"
+
+def get_ai_response(client: OpenAI, chat_history: list, context:str):
+    #built the prompt
+    ai_prompt = build_ai_prompt(context)
+    ai_prompt_message= [
+        {
+            "role": "system",
+            "content": ai_prompt
+        }
+    ]
+    messages = chat_history + ai_prompt_message
+
+        #call the open ai agent, get the response 
+    ai_response = client.chat.completions.create(
+        model="gpt-5-mini",
+        messages=messages,
+        temperature=1
+    )
+
+    return ai_response.choices[0].message.content
 
 logs = load_logs("ai-assistant/ai_logs.json")
 
-for log in logs:
-    st.session_state['messages'].append(
-        {
-            'role': log['role'],
-            'content': log['context']
-        }
-    )
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+    for log in logs:
+        st.session_state['messages'].append(
+            {
+                'role': log['role'],
+                'content': log['content']
+            }
+        )
+    if len(st.session_state['messages']) == 0:
+        st.session_state['messages'].append(
+            {
+                'role' : 'assistant',
+                'content' : "Hi , ask me a question"
+            }
+        )
 
-if len(st.session_state['messages']) == 0:
+
+for message in st.session_state['messages']:
+    with st.chat_message(message['role']):
+        st.markdown(message['content'])
+
+
+user_input = st.chat_input("Ask me a question...")
+
+if user_input:
     st.session_state['messages'].append(
         {
-            'role' : 'ai-assistent',
-            'content' : "Hi , ask me a question"
-        }
+            "role": "user",
+            "content": user_input
+    }
     )
+    with st.chat_message('user'):
+        st.markdown(user_input)
+    
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking...."):
+            import time
+            time.sleep(2)
+            ai_response = get_ai_response(clinet, st.session_state['messages'], "my context is healthcare")
+            st.markdown(ai_response)
+
+
+        st.session_state['messages'].append(
+            {
+                'role': 'assistant',
+                'content': ai_response
+            }
+        )
+
+    logs = load_logs("ai-assistant/ai_logs.json")
+    logs.append({
+        'user_message':user_input,
+        'ai_response': ai_response
+    })
+    save_logs("ai-assistant/ai_logs.json", logs)
